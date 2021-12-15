6439035849690562bfe7612d87baef5d910486fa
reifab <72970313+reifab@users.noreply.github.com>
2021-12-15 15:59:25 +0100

    simple test

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+gatorLog.beginWithCustomPins(SerialPin.P8, SerialPin.P12, DigitalPin.P13)
+gatorLog.setDateAndTime(
+2021,
+12,
+15,
+15,
+30
+)
+gatorLog.mkDirectory("Testfolder")
+gatorLog.chDirectory("Testfolder")
+gatorLog.openCSVFile("Testfile")
+gatorLog.writeRowWithTextToCSV(["Temperature °C", "Lightintensity"], HeaderLine.YES)
 basic.forever(function () {
-	
+    gatorLog.writeRowWithNumbersToCSV([input.temperature(), input.lightLevel()], HeaderLine.NO)
+    basic.pause(1000)
 })
